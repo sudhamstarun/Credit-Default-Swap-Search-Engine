7caea96830dbbb75585316567bcedf4942e69b87
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-02-18 15:54:51 +0800

    add url patterns more to come

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^backend/', include('backend.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
