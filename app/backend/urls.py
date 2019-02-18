2285dabd69c90ee1a7bdca45492485efd5adfc68
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-02-18 16:12:12 +0800

    typo fixes

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from backend import views
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 
 urlpatterns = [
+    #  Generic urls views added
     url(r'^admin/', include(admin.site.urls)),
     url(r'^backend/', include('backend.urls')),
+
+    # Specific URL and Views
+
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
