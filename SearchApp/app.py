1368060b37292b3d0101e279d438e6296a6f4ac3
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-04-01 12:15:51 +0800

    take input

--- SearchApp/app.py
+++ SearchApp/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
+@app.route("/form", methods=["GET"])
+def get_form():
+    return render_template('index.html')
+
+
+@app.route('/input', methods=['GET', 'POST'])
+def takeInput():
+    if request.method == 'POST':
+        input = request.form['text']
+    print(input)
+
     return render_template("index.html")
 
 
