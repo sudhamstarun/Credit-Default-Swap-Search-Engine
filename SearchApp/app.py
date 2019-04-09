a297fafd3509cc642cd1d550b6a258b6651bbe9a
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-04-10 01:04:36 +0800

    unified view up and running

--- SearchApp/app.py
+++ SearchApp/app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, send_from_directory
 from json2html import *
 
 import os
-import pandas
+import pandas as pd
 import glob
 import csv
 import json
 import re
 
+pd.set_option('display.max_columns', None)
 parent_dir = "../Data/"
 filelist = []
 
@@ -49,15 +50,26 @@ def searchJsonArray(search_string):
 
 
 def search_csv(search_string):
-    csv_file = pd.read_csv('../UnifiedCSV/final_csv.csv')
+    csv_file = pd.read_csv('../UnifiedCSV/final_csv.csv',
+                           dtype=str)
 
+    list_columns = list(csv_file)
     num_columns = len(list(csv_file))
-    # loop through csv list
-    for row in csv_file:
-        # Search
-        for iterator in range(0, num_columns):
-            if number == row[]:
-                print row
+    results_rows = []
+    csvreader_file = csv.reader(
+        open('../UnifiedCSV/final_csv.csv', "r"), delimiter=",")
+
+    print(csvreader_file)
+    for row in csvreader_file:
+        for iterator in range(num_columns):
+            if search_string in row[iterator]:
+                results_rows.append(row)
+
+    search_results = pd.DataFrame(
+        results_rows, columns=list_columns, index=None)
+
+    html_tags = search_results.to_html()
+    return html_tags
 
 
 app = Flask(__name__)
@@ -88,10 +100,10 @@ def printJsonResults():
             return render_template('raw_result.html', results=tags)
 
     if 'button2' in request.form:
-        tags = []
         if request.method == 'POST':
             search_string = request.form['input']
-            results = searchJsonArray(search_string)
+            result = search_csv(search_string)
+            return render_template('unified_results.html', results=result)
 
     return '', 204
 
