db40706759ba039250043060f56bb277d6174396
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-04-18 18:12:00 +0800

    fix requirements issues on multiple machines

--- SearchApp/app.py
+++ SearchApp/app.py
@@ -143,19 +143,19 @@ def upload():
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            return redirect('/redirect')
+            return '', 204
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             flash('No selected file')
-            return redirect('/redirect')
+            return '', 204
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('extractReport'))
 
-        return render_template("index.html")
+        return '', 204
 
 
 @app.route('/extractResults', methods=['GET'])
